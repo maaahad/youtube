89cd5351ef53160b699c45989f318d1ae9466ad5
Muhammed  Ahad <maaahad@gmail.com>
2025-04-08 10:11:24 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="pageContainer">
+			<div className="header">
+				<div>LeftSlot</div>
+				<div className="centerSlot">CenterSlot</div>
+				<div>RightSlot</div>
+			</div>
+			<div className="content">
+				<div className="hero">Hero Section </div>
+				<div className="otherComponent"> Other Components </div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
